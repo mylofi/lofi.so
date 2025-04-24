0df44b33e5986109021ef6e92bc795fecb2b4d94
Dagmawi-Y <dagmawi.inbox@gmail.com>
2025-04-24 10:03:09 +0300

    chore: update adapter configuration in svelte.config.js and remove process.env definition in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,5 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
-	define: {
-		'process.env': {
-			NODE_ENV: process.env.NODE_ENV || 'production'
-		}
-	}
+	define: {}
 });
