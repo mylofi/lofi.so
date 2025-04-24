3dc33a3001c2b6389b64c0ecbf516cc62675baec
Dagmawi-Y <dagmawi.inbox@gmail.com>
2025-04-24 09:56:26 +0300

    chore: process.env definition

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import { defineConfig } from 'vite';
 export default defineConfig({
 	plugins: [sveltekit()],
 	define: {
-		'process.env': {}
+		'process.env': {
+			NODE_ENV: process.env.NODE_ENV || 'production'
+		}
 	}
 });
